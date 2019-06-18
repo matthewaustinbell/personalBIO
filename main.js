ec8f85e7d854cac0822331c18d009872380fdff1
Matt Bell <matthewaustinbell@gmail.com>
2019-06-18 12:53:22 -0500

    wip

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ const projects = [{
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    url: "https://github.com/matthewaustinbell/tamogachi3", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/matthewaustinbell/tamogachi3"
 }, {
     title: "Cool Project2",
     screenshot: "https://vignette.wikia.nocookie.net/joke-battles/images/7/72/12198090531909861341man-silhouette-svg-hi12.png/revision/latest?cb=20161120035851",
